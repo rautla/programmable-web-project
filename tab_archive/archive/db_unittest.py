eecc6c0a16b05d5a392a741977a8bd581bf4d602
Ville Kojola <ville.kojola@gmail.com>
2013-04-12 10:56:43 +0300

    unittest

--- tab_archive/archive/db_unittest.py
+++ tab_archive/archive/db_unittest.py
@@ -7,13 +7,27 @@ from archive.models import UserModel, TablatureModel, CommentModel
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
+        '''
+        Makes sure that tables exist.
+        '''
         db.create_users_table()
         db.create_tablatures_table()
         db.create_comments_table()
 
     def test_add_user(self):
-        user_info = {}
-        db.database.add_user()
+        '''
+        Try to create user.
+        '''
+        user = UserModel.create({"user_nickname":"erkki" , "email":"erkki@sposti.org", "description":,"Hodor"})
+        name = db.database.add_user(user)
+        self.assertEqual(name, "erkki")
+        
+    def test_add_user_fail(self):
+        '''
+        Try to create user with nickname that already exists.
+        '''
+        user = UserModel.create({"user_nickname":"erkki", "email":"erkki@ekspertti.info", "description":""})
+       
             
         
 if __name__ == '__main__':
