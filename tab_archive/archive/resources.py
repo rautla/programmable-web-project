663a1fc2e9f37e797bd0b9505a788d45e98e8096
Riku Klemetti <rklemett@st-cn0001.oulu.fi>
2013-04-19 15:29:10 +0300

    resources.py modified

--- tab_archive/archive/resources.py
+++ tab_archive/archive/resources.py
@@ -21,7 +21,7 @@ class User(APIView):
         if self._isauthorized(user_nickname, authorization):
             return self._readauthorized(request, user_nickname) 
         else:
-            return self._readunathorized(request, user_nickname) 
+            return self._readunauthorized(request, user_nickname) 
     
     def delete(self, request, user_nickname):
         authorization = ''
@@ -45,6 +45,7 @@ class User(APIView):
         else:
             return self._createuser(request, user_nickname)
     
+    def _readunathorized(self, request, user_nickname):
         #Use the database to extract a user information. Use the method 
         #database.getUser(user_id) to obtain a UserModel
         usermodel = database.get_user(user_nickname)
@@ -59,14 +60,14 @@ class User(APIView):
         output = {}
         #Create the dictionaries for publicProfile, history and users. To avoid
         #problems transform the registrationdata and description in string using the str() function
-        publicprofile = {'user_nickname':usermodel.user_nickname,
+        publicprofile = {'nickname':usermodel.user_nickname,
                        'picture':usermodel.picture,
                        'description':str(usermodel.description)}
         users = {'rel':'self', 'href':uritousers}
         #Append to the output
         output['publicprofile'] = publicprofile
         output['users'] = users
-        return Response(output, status=status.HTTP_200_OK)
+        return Response(output, status=status.HTTP_200_OK)    
     
     def _readauthorized(self, request, user_nickname):
         usermodel = database.get_user(user_nickname)
@@ -297,14 +298,14 @@ class Comment(APIView):
         #If replyto exists, include the url of the reply comment
         replytocomment_url = None
         if 'reply_to' in comment:
-            replytocomment_url = "http://localhost:8000/tab_archive/tablatures/" + comment[tablature_id] + "/" + comment[comment_id])
+            replytocomment_url = "http://localhost:8000/tab_archive/tablatures/" + comment[tablature_id] + "/" + comment[comment_id]
             replytocomment_url = reverse("comment", (comment['reply_to'],), 
                                          request=request)
             comment['reply_to'] = replytocomment_url
         return Response(comment, status=status.HTTP_200_OK)    
     
     def delete(self, request, comment_id):
-       try:
+        try:
             if database.delete_comment(comment_id):
                 return Response(None, status=status.HTTP_204_NO_CONTENT)
             else:
@@ -358,12 +359,11 @@ class Comment(APIView):
             print "Could not add the data "+ str(e)
             traceback.print_exc()
             return Response(status = 400)
-        commentmodel.reply_to = comment_id '''TÄÄLLÄ VOI OLLA MÄTÄÄ'''
+        commentmodel.reply_to = comment_id #TÄÄLLÄ VOI OLLA MÄTÄÄ
         database.add_comment(commentmodel)
         url = reverse("comment", (comment_id,), request=request)
         return Response(status=status.HTTP_204_NO_CONTENT,
-                        headers={"Location":url})
-    
+                        headers={"Location":url})   
 
 class Rating(APIView):
     '''HUOM HOX MITES TÄMÄ'''
@@ -373,7 +373,7 @@ class Rating(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)        
         #Rating output looks: 
         #[{'rating':rating, 'link':{'rel':'self','href'=:'http://tab_archive/tablatures/tablature_id/rating'}}]
-        rating[]
+
         _rating = tablaturemodel.rating
         _ratingurl = "http://localhost:8000/tab_archive/tablatures/" + tablature[tablature_id] + "/" + tablature[rating]
         _ratingurl = reverse("rating", (_rating,), request=request)
@@ -405,8 +405,7 @@ class Rating(APIView):
             return Response(status=status.HTTP_400_BAD_REQUEST)
         '''JOS EI TOIMI TÄÄLLÄ ON VIKA''' 
         
-        return Response(rating[0], status=status.HTTP_200_OK)
-    
+        return Response(rating[0], status=status.HTTP_200_OK)   
 
 class Tablature(APIView):
     def get (self, request, tablature_id):
